f90b0124c52590b7b09e9acf6c8721d248f99d1a
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-27 17:53:56 +0530

    Create BAISED.cpp

---
+++ BAISED.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long mod = 1e9+7;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--) {
+    	long long n, sum = 0;
+    	string str;
+    	cin >> n;
+    	vector<long long> v(n);
+    	for (int i = 0; i < n; i++)
+    		cin >> str >> v[i];
+    	sort(v.begin(), v.end());
+    	for (int i = 0; i < n; i++)
+    		sum += abs((i+1) - v[i]);
+    	cout << sum << endl;
+    }
+}

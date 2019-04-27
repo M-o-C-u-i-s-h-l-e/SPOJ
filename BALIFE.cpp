608693771063b46abb89a83f9241c4cfe42f015d
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-27 17:55:02 +0530

    Create BALIFE.cpp

---
+++ BALIFE.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long mod = 1e9+7;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+#endif
+
+    int n;
+    while (cin >> n) {
+    	if (n == -1)
+    		break;
+    	vector<int> v(n), pre(n), org(n);
+    	for (int i = 0; i < n; i++)
+    		cin >> v[i];
+    	int sum = accumulate(v.begin(), v.end(), 0);
+    	if (sum % n != 0) {
+    		cout << -1 << endl;
+    		continue;
+    	}
+    	pre[0] = v[0];
+    	int avg = sum / n;
+    	org[0] = avg;
+    	for (int i = 1; i < n; i++)
+    		pre[i] = pre[i-1] + v[i], org[i] = org[i-1] + avg;
+    	int Max = INT_MIN;
+    	for (int i = 0; i < n; i++)
+    		Max = max(Max, abs(pre[i] - org[i]));
+    	cout << Max << endl;
+    }
+}

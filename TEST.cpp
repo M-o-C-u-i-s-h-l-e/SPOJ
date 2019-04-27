33e6e183a0d2bd28332ec6acb479ad99e4637c23
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-27 18:01:31 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	while (cin >> n) {
+		if (n == 42)	break;
+		cout << n << endl;
+	}
+}

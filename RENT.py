f0e30e4d1f496d57bb74802bb892d1a6ecd00d62
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-30 23:32:44 +0530

    Create RENT.py

---
+++ RENT.py
@@ -0,0 +1,16 @@
+import bisect as bs
+
+for _ in range(int(input())):
+	n = int(input())
+	ls = []
+	for i in range(n):
+		temp = list(map(int, input().split()))
+		ls.append(temp)
+	ls.sort()
+	s = [ls[i][0] for i in range(n)]
+	d = [ls[i][1] for i in range(n)]
+	c = [ls[i][2] for i in range(n)]
+	dp = [0] * (n+1)
+	for i in range(n-1, -1, -1):
+		dp[i] = max(dp[i+1], dp[bs.bisect_right(s, s[i]+d[i])] + c[i])
+	print(max(dp))

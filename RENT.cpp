381a789b41a6125c6b0b8197e7f56385aca1d3e4
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-30 23:32:10 +0530

    Create RENT.cpp

---
+++ RENT.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define F first
+#define S second
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<pair<pair<int, int>, int>> v(n);
+		vector<int> s(n), e(n), c(n), dp(n+1, 0);
+		for (int i = 0; i < n; i++)
+			cin >> v[i].F.F >> v[i].F.S >> v[i].S;
+		sort(v.begin(), v.end());
+		for (int i = 0; i < n; i++)
+			s[i] = v[i].F.F, e[i] = v[i].F.S, c[i] = v[i].S;
+		for (int i = n-1; i >= 0; i--)
+			dp[i] = max(dp[i+1], c[i] + dp[(upper_bound(s.begin(), s.end(), s[i]+e[i]) - s.begin())]);
+		cout << *max_element(dp.begin(), dp.end()) << endl;
+	}
+}
